3086ef147a6c7201f5315f29b4b9a7e2bab0cc03
José Victor <jvictormmendesd@gmail.com>
2023-03-29 20:44:50 -0300

    subindo código completo

--- nba/src/App.java
+++ nba/src/App.java
@@ -1,6 +1,73 @@
+import java.util.Scanner;
+
 public class App {
+    static Scanner s = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        Jogador teste = new Jogador("0,Curly Armstrong,180,77,Indiana University,1918,,");
-        System.out.println(teste.getId());
+        MyIO.setCharset("UTF-8");
+
+        // lê os dados dos jogadores
+        Jogador[] jogadores = lerJogadores();
+
+        String numLinhas = s.nextLine();
+
+        s.nextLine();
+
+        // Array com ids
+        String[] ids = new String[Integer.parseInt(numLinhas)];
+
+        for (int i = 0; i < ids.length; i++) {
+            try {
+                ids[i] = s.nextLine();
+            } catch (Exception e) {
+            }
+        }
+
+        s.close();
+
+        for (String i : ids) {
+            if (i != null && !i.isEmpty()) {
+                for (Jogador jogador : jogadores) {
+                    if (jogador.getId() == Integer.parseInt(i)) {
+                        jogador.imprimir();
+                    }
+                }
+            }
+        }
+
+    }
+
+    private static Jogador[] lerJogadores() {
+        // Scanner s = new Scanner(System.in);
+
+        // lê os dados dos jogadores e adiciona ao vetor de jogadores
+        Jogador[] jogadores = new Jogador[20000];
+        int numJogadores = 0;
+        String linha = s.nextLine();
+
+        while (!linha.equals("FIM")) {
+            Jogador jogador = new Jogador(linha);
+
+            jogadores[numJogadores] = jogador;
+            numJogadores++;
+
+            linha = s.nextLine();
+        }
+
+        // s.nextLine();
+        // s.close();
+
+        // cria um novo vetor de jogadores com o tamanho exato do número de jogadores
+        // lidos
+        Jogador[] jogadoresFinais = new Jogador[numJogadores];
+        for (int i = 0; i < numJogadores; i++) {
+            jogadoresFinais[i] = jogadores[i];
+        }
+
+        // for (Jogador jogador : jogadoresFinais) {
+        // jogador.imprimir();
+        // }
+
+        return jogadoresFinais;
     }
 }
--- nba/src/Jogador.java
+++ nba/src/Jogador.java
@@ -28,11 +28,82 @@ public class Jogador {
 
     public Jogador(String packet) {
         StringTokenizer aux = new StringTokenizer(packet, ",");
+
         // while (aux.hasMoreTokens()) {
-            // System.out.println(aux.nextToken());
-            this.id = Integer.parseInt(aux.nextToken());
+        // System.out.println(aux.nextToken());
+        // System.out.println();
         // }
-        // System.prin
+
+        // System.out.println();
+        // System.out.println();
+
+        /*
+         * - id do jogador;
+         * - nome do jogador;
+         * - sua altura;
+         * - seu peso;
+         * - universidade que o jogador representa;
+         * - ano de nascimento do jogador;
+         * - nome da cidade em que o jogador nasceu;
+         * - estado em que o jogador nasceu.
+         */
+
+        try {
+            this.id = Integer.parseInt(aux.nextToken());
+        } catch (Exception e) {
+            if (aux.hasMoreTokens()) {
+                aux.nextToken();
+            }
+        }
+        try {
+            this.nome = aux.nextToken();
+        } catch (Exception e) {
+            if (aux.hasMoreTokens()) {
+                aux.nextToken();
+            }
+        }
+        try {
+            this.altura = Integer.parseInt(aux.nextToken());
+        } catch (Exception e) {
+            if (aux.hasMoreTokens()) {
+                aux.nextToken();
+            }
+        }
+        try {
+            this.peso = Integer.parseInt(aux.nextToken());
+        } catch (Exception e) {
+            if (aux.hasMoreTokens()) {
+                aux.nextToken();
+            }
+        }
+        try {
+            this.universidade = aux.nextToken();
+        } catch (Exception e) {
+            if (aux.hasMoreTokens()) {
+                aux.nextToken();
+            }
+        }
+        try {
+            this.anoNascimento = Integer.parseInt(aux.nextToken());
+        } catch (Exception e) {
+            if (aux.hasMoreTokens()) {
+                aux.nextToken();
+            }
+        }
+        try {
+            this.cidadeNascimento = aux.nextToken();
+        } catch (Exception e) {
+            if (aux.hasMoreTokens()) {
+                aux.nextToken();
+            }
+        }
+        try {
+            this.estadoNascimento = aux.nextToken();
+        } catch (Exception e) {
+            if (aux.hasMoreTokens()) {
+                aux.nextToken();
+            }
+        }
     }
 
     // getters
@@ -103,13 +174,51 @@ public class Jogador {
 
     // Outros métodos
     public void imprimir() {
-        System.out.println(this.id);
-        System.out.println(this.nome);
-        System.out.println(this.altura);
-        System.out.println(this.peso);
-        System.out.println(this.universidade);
-        System.out.println(this.anoNascimento);
-        System.out.println(this.cidadeNascimento);
-        System.out.println(this.estadoNascimento);
+        System.out.print("[" + this.id);
+
+        if (this.nome != null && !this.nome.isEmpty()) {
+            System.out.print(" ## " + this.nome);
+        } else {
+            System.out.print(" ## nao informado");
+        }
+
+        if (this.altura != 0.0) {
+            System.out.print(" ## " + this.altura);
+        } else {
+            System.out.print("## nao informado");
+        }
+
+        if (this.peso != 0.0) {
+            System.out.print(" ## " + this.peso);
+        } else {
+            System.out.print("## nao informado");
+        }
+
+        if (this.anoNascimento != 0) {
+            System.out.print(" ## " + this.anoNascimento);
+        } else {
+            System.out.print(" ## nao informado");
+        }
+
+        if (this.universidade != null && !this.universidade.isEmpty()) {
+            System.out.print(" ## " + this.universidade);
+        } else {
+            System.out.print(" ## nao informado");
+        }
+
+        if (this.cidadeNascimento != null && !this.cidadeNascimento.isEmpty()) {
+            System.out.print(" ## " + this.cidadeNascimento);
+        } else {
+            System.out.print(" ## nao informado");
+        }
+
+        if (this.estadoNascimento != null && !this.estadoNascimento.isEmpty()) {
+            System.out.print(" ## " + this.estadoNascimento);
+        } else {
+            System.out.print(" ## nao informado");
+        }
+
+        System.out.println("]");
+
     }
 }
