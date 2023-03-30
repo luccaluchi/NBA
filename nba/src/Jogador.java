4475da2db7d47752dcc062934e8b8626aabff0f5
José Victor <jvictormmendesd@gmail.com>
2023-03-29 21:40:55 -0300

    passando tudo para App e corrigindo erro

--- nba/src/Jogador.java
+++ nba/src/Jogador.java
@@ -1,224 +1,204 @@
 import java.util.StringTokenizer;
 
-public class Jogador {
-    private int id;
-    private String nome;
-    private int altura;
-    private int peso;
-    private String universidade;
-    private int anoNascimento;
-    private String cidadeNascimento;
-    private String estadoNascimento;
+// public class Jogador {
+//     private int id;
+//     private String nome;
+//     private int altura;
+//     private int peso;
+//     private String universidade;
+//     private int anoNascimento;
+//     private String cidadeNascimento;
+//     private String estadoNascimento;
 
-    // Construtores da classe
-    public Jogador() {
-    }
+//     // Construtores da classe
+//     public Jogador() {
+//     }
 
-    public Jogador(int id, String nm, int altura, int peso, String unv, int aNas,
-            String cNas, String eNas) {
-        this.id = id;
-        this.nome = nm;
-        this.altura = altura;
-        this.peso = peso;
-        this.universidade = unv;
-        this.anoNascimento = aNas;
-        this.cidadeNascimento = cNas;
-        this.estadoNascimento = eNas;
-    }
+//     public Jogador(int id, String nm, int altura, int peso, String unv, int aNas,
+//             String cNas, String eNas) {
+//         this.id = id;
+//         this.nome = nm;
+//         this.altura = altura;
+//         this.peso = peso;
+//         this.universidade = unv;
+//         this.anoNascimento = aNas;
+//         this.cidadeNascimento = cNas;
+//         this.estadoNascimento = eNas;
+//     }
 
-    public Jogador(String packet) {
-        StringTokenizer aux = new StringTokenizer(packet, ",");
+//     public Jogador(String packet) {
+//         String[] tokens = packet.split(",", -1);
+//         // - id do jogador;
+//         // - nome do jogador;
+//         // - sua altura;
+//         // - seu peso;
+//         // - universidade que o jogador representa;
+//         // - ano de nascimento do jogador;
+//         // - nome da cidade em que o jogador nasceu;
+//         // - estado em que o jogador nasceu.
 
-        // while (aux.hasMoreTokens()) {
-        // System.out.println(aux.nextToken());
-        // System.out.println();
-        // }
+//         try {
+//             this.id = Integer.parseInt(tokens[0]);
+//         } catch (Exception e) {
+//             // handle exception
+//         }
 
-        // System.out.println();
-        // System.out.println();
+//         try {
+//             this.nome = tokens[1];
+//         } catch (Exception e) {
+//             // handle exception
+//         }
 
-        /*
-         * - id do jogador;
-         * - nome do jogador;
-         * - sua altura;
-         * - seu peso;
-         * - universidade que o jogador representa;
-         * - ano de nascimento do jogador;
-         * - nome da cidade em que o jogador nasceu;
-         * - estado em que o jogador nasceu.
-         */
+//         try {
+//             this.altura = Integer.parseInt(tokens[2]);
+//         } catch (Exception e) {
+//             // handle exception
+//         }
 
-        try {
-            this.id = Integer.parseInt(aux.nextToken());
-        } catch (Exception e) {
-            if (aux.hasMoreTokens()) {
-                aux.nextToken();
-            }
-        }
-        try {
-            this.nome = aux.nextToken();
-        } catch (Exception e) {
-            if (aux.hasMoreTokens()) {
-                aux.nextToken();
-            }
-        }
-        try {
-            this.altura = Integer.parseInt(aux.nextToken());
-        } catch (Exception e) {
-            if (aux.hasMoreTokens()) {
-                aux.nextToken();
-            }
-        }
-        try {
-            this.peso = Integer.parseInt(aux.nextToken());
-        } catch (Exception e) {
-            if (aux.hasMoreTokens()) {
-                aux.nextToken();
-            }
-        }
-        try {
-            this.universidade = aux.nextToken();
-        } catch (Exception e) {
-            if (aux.hasMoreTokens()) {
-                aux.nextToken();
-            }
-        }
-        try {
-            this.anoNascimento = Integer.parseInt(aux.nextToken());
-        } catch (Exception e) {
-            if (aux.hasMoreTokens()) {
-                aux.nextToken();
-            }
-        }
-        try {
-            this.cidadeNascimento = aux.nextToken();
-        } catch (Exception e) {
-            if (aux.hasMoreTokens()) {
-                aux.nextToken();
-            }
-        }
-        try {
-            this.estadoNascimento = aux.nextToken();
-        } catch (Exception e) {
-            if (aux.hasMoreTokens()) {
-                aux.nextToken();
-            }
-        }
-    }
+//         try {
+//             this.peso = Integer.parseInt(tokens[3]);
+//         } catch (Exception e) {
+//             // handle exception
+//         }
 
-    // getters
-    public int getId() {
-        return id;
-    }
+//         try {
+//             this.universidade = tokens[4];
+//         } catch (Exception e) {
+//             // handle exception
+//         }
 
-    public String getNome() {
-        return nome;
-    }
+//         try {
+//             this.anoNascimento = Integer.parseInt(tokens[5]);
+//         } catch (Exception e) {
+//             // handle exception
+//         }
 
-    public int getAltura() {
-        return altura;
-    }
+//         try {
+//             this.cidadeNascimento = tokens[6];
+//         } catch (Exception e) {
+//             // handle exception
+//         }
 
-    public int getPeso() {
-        return peso;
-    }
+//         try {
+//             this.estadoNascimento = tokens[7];
+//         } catch (Exception e) {
+//             // handle exception
+//         }
+//     }
 
-    public String getUniversidade() {
-        return universidade;
-    }
+//     // getters
+//     public int getId() {
+//         return id;
+//     }
 
-    public int getAnoNascimento() {
-        return anoNascimento;
-    }
+//     public String getNome() {
+//         return nome;
+//     }
 
-    public String getCidadeNascimento() {
-        return cidadeNascimento;
-    }
+//     public int getAltura() {
+//         return altura;
+//     }
 
-    public String getEstadoNascimento() {
-        return estadoNascimento;
-    }
+//     public int getPeso() {
+//         return peso;
+//     }
 
-    // setters
-    public void setId(int id) {
-        this.id = id;
-    }
+//     public String getUniversidade() {
+//         return universidade;
+//     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+//     public int getAnoNascimento() {
+//         return anoNascimento;
+//     }
 
-    public void setAltura(int altura) {
-        this.altura = altura;
-    }
+//     public String getCidadeNascimento() {
+//         return cidadeNascimento;
+//     }
 
-    public void setPeso(int peso) {
-        this.peso = peso;
-    }
+//     public String getEstadoNascimento() {
+//         return estadoNascimento;
+//     }
 
-    public void setUniversidade(String universidade) {
-        this.universidade = universidade;
-    }
+//     // setters
+//     public void setId(int id) {
+//         this.id = id;
+//     }
 
-    public void setAnoNascimento(int anoNascimento) {
-        this.anoNascimento = anoNascimento;
-    }
+//     public void setNome(String nome) {
+//         this.nome = nome;
+//     }
 
-    public void setCidadeNascimento(String cidadeNascimento) {
-        this.cidadeNascimento = cidadeNascimento;
-    }
+//     public void setAltura(int altura) {
+//         this.altura = altura;
+//     }
 
-    public void setEstadoNascimento(String estadoNascimento) {
-        this.estadoNascimento = estadoNascimento;
-    }
+//     public void setPeso(int peso) {
+//         this.peso = peso;
+//     }
 
-    // Outros métodos
-    public void imprimir() {
-        System.out.print("[" + this.id);
+//     public void setUniversidade(String universidade) {
+//         this.universidade = universidade;
+//     }
 
-        if (this.nome != null && !this.nome.isEmpty()) {
-            System.out.print(" ## " + this.nome);
-        } else {
-            System.out.print(" ## nao informado");
-        }
+//     public void setAnoNascimento(int anoNascimento) {
+//         this.anoNascimento = anoNascimento;
+//     }
 
-        if (this.altura != 0.0) {
-            System.out.print(" ## " + this.altura);
-        } else {
-            System.out.print("## nao informado");
-        }
+//     public void setCidadeNascimento(String cidadeNascimento) {
+//         this.cidadeNascimento = cidadeNascimento;
+//     }
 
-        if (this.peso != 0.0) {
-            System.out.print(" ## " + this.peso);
-        } else {
-            System.out.print("## nao informado");
-        }
+//     public void setEstadoNascimento(String estadoNascimento) {
+//         this.estadoNascimento = estadoNascimento;
+//     }
 
-        if (this.anoNascimento != 0) {
-            System.out.print(" ## " + this.anoNascimento);
-        } else {
-            System.out.print(" ## nao informado");
-        }
+//     // Outros métodos
+//     public void imprimir() {
+//         System.out.print("[" + this.id);
 
-        if (this.universidade != null && !this.universidade.isEmpty()) {
-            System.out.print(" ## " + this.universidade);
-        } else {
-            System.out.print(" ## nao informado");
-        }
+//         if (this.nome != null && !this.nome.isEmpty()) {
+//             System.out.print(" ## " + this.nome);
+//         } else {
+//             System.out.print(" ## nao informado");
+//         }
 
-        if (this.cidadeNascimento != null && !this.cidadeNascimento.isEmpty()) {
-            System.out.print(" ## " + this.cidadeNascimento);
-        } else {
-            System.out.print(" ## nao informado");
-        }
+//         if (this.altura != 0.0) {
+//             System.out.print(" ## " + this.altura);
+//         } else {
+//             System.out.print("## nao informado");
+//         }
 
-        if (this.estadoNascimento != null && !this.estadoNascimento.isEmpty()) {
-            System.out.print(" ## " + this.estadoNascimento);
-        } else {
-            System.out.print(" ## nao informado");
-        }
+//         if (this.peso != 0.0) {
+//             System.out.print(" ## " + this.peso);
+//         } else {
+//             System.out.print("## nao informado");
+//         }
 
-        System.out.println("]");
+//         if (this.anoNascimento != 0) {
+//             System.out.print(" ## " + this.anoNascimento);
+//         } else {
+//             System.out.print(" ## nao informado");
+//         }
 
-    }
-}
+//         if (this.universidade != null && !this.universidade.isEmpty()) {
+//             System.out.print(" ## " + this.universidade);
+//         } else {
+//             System.out.print(" ## nao informado");
+//         }
+
+//         if (this.cidadeNascimento != null && !this.cidadeNascimento.isEmpty()) {
+//             System.out.print(" ## " + this.cidadeNascimento);
+//         } else {
+//             System.out.print(" ## nao informado");
+//         }
+
+//         if (this.estadoNascimento != null && !this.estadoNascimento.isEmpty()) {
+//             System.out.print(" ## " + this.estadoNascimento);
+//         } else {
+//             System.out.print(" ## nao informado");
+//         }
+
+//         System.out.println("]");
+
+//     }
+// }
